afdb585ec38775fefc4436ad732651bfde30a335
tejas wadeyar <Tejaswdeyar142@gmail.com>
2024-03-16 17:40:20 +0530

    shell code function

--- 04 functions.sh
+++ 04 functions.sh
@@ -1,5 +1,13 @@
 sample1() {
-echo "Hello World"
+#Inside function code
+ echo "Hello World"
 }
 
+#main function
 sample1
+
+sample_sum() {
+  echo "Addition of $1 and $2 is $(($1+$2))"
+  }
+
+  sample_sum 200 400
