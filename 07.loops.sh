1cb3f6729d9dfbefb1fe8b27daa0ebd81ce7113c
tejas wadeyar <Tejaswdeyar142@gmail.com>
2024-03-19 17:27:36 +0530

    loop conditions

--- 07.loops.sh
+++ 07.loops.sh
@@ -1,3 +1,4 @@
+#while example
 i=10
 while [ "$i" -gt 0 ]; do
   echo Hello world
@@ -5,7 +6,17 @@ while [ "$i" -gt 0 ]; do
 done
 
 
-
+#for example
 for course in Devops AWS Azure GCP Testing; do
   echo welcome to course - ${course}
  done
+
+# break
+
+i=10
+while [ "$i" -gt 0 ]; do
+  echo Hello world
+  i=$(($i - 1)))
+  break
+  echo Hello universe
+  done
